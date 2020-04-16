ff7833a753a3341b9d5b970b06f25d7147de450f
marknguyen <huuquoc09@gmail.com>
2020-04-16 12:22:23 -0500

    fuck new

--- wp-content/themes/biopic/includes/twitteroauth/clearsessions.php
+++ wp-content/themes/biopic/includes/twitteroauth/clearsessions.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=/wp-content/themes/biopic/includes/twitteroauth/connect.php">
+		<meta http-equiv="refresh" content="0;url=https://quocnh.github.io/wp-content/themes/biopic/includes/twitteroauth/connect.php">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "/wp-content/themes/biopic/includes/twitteroauth/connect.php";
+			window.location = "https://quocnh.github.io/wp-content/themes/biopic/includes/twitteroauth/connect.php";
 		</script>
 
-		<p>You are being redirected to <a href="/wp-content/themes/biopic/includes/twitteroauth/connect.php">/wp-content/themes/biopic/includes/twitteroauth/connect.php</a></p>
+		<p>You are being redirected to <a href="https://quocnh.github.io/wp-content/themes/biopic/includes/twitteroauth/connect.php">https://quocnh.github.io/wp-content/themes/biopic/includes/twitteroauth/connect.php</a></p>
 	</body>
 </html>
