01ed4881a7ab361603e5c41f4941a3b831f792f5
marknguyen <huuquoc09@gmail.com>
2020-04-16 12:07:27 -0500

    new website - home

---
+++ wp-content/themes/biopic/includes/twitteroauth/clearsessions.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/wp-content/themes/biopic/includes/twitteroauth/connect.php">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/wp-content/themes/biopic/includes/twitteroauth/connect.php";
+		</script>
+
+		<p>You are being redirected to <a href="/wp-content/themes/biopic/includes/twitteroauth/connect.php">/wp-content/themes/biopic/includes/twitteroauth/connect.php</a></p>
+	</body>
+</html>
